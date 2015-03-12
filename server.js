b1795c8e9b42e18ef0d1ae3801cc518b93914fbf
Michal Tomeczek | hybris <michal.tomeczek@hybris.com>
2015-03-13 00:52:09 +0100

    Initial push

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {  
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('hello, i know nodejitsu.')
+  res.end();
+}).listen(8000); 
+console.log('> hello world running on port 8000');
